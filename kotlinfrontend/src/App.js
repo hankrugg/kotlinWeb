fbe14725603856d81d8eb8d1e685792ae0697b90
hankrugg <hankrugg2@gmail.com>
2024-03-01 12:04:20 -0700

    add frontend

---
+++ kotlinfrontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
